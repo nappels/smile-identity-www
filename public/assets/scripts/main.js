4b45218caee71790c80dad58f8585d6424d00f79
Noah Appel <noah@Noahs-MacBook-Pro.local>
2017-03-20 18:44:56 -0400

    Styles and dummy content.

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,3 +1,15 @@
 //
 // Main JavaScript file
 // --------------------------------------------------------
+
+function setHeader() {
+  if (window.scrollY > 100) {
+    document.body.classList.add('scrolled');
+  } else {
+    document.body.classList.remove('scrolled');
+  }
+}
+
+window.addEventListener("scroll", function(evt) {
+  setHeader();
+});
