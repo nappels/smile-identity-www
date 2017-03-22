8086e7dadcb52e2aeb89d1944cebfac92b8be296
Noah Appel <noah.appel@gmail.com>
2017-03-22 11:47:24 -0400

    Wired up email form to formspree. Other small tweaks.

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -2,6 +2,8 @@
 // Main JavaScript file
 // --------------------------------------------------------
 
+// Header Scroll
+// -------------------------------------------
 function setHeader() {
   if (window.scrollY > 100) {
     document.body.classList.add('scrolled');
@@ -12,4 +14,29 @@ function setHeader() {
 
 window.addEventListener("scroll", function(evt) {
   setHeader();
+});
+
+
+// Form submit
+// -------------------------------------------
+
+document.querySelector('.contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  var email = document.querySelector('.email-input').value;
+
+  fetch('https://formspree.io/info@smileidentity.com', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: email
+    })
+  }).then(function(resp) {
+    return resp.json();
+  }).then(function(json) {
+    document.querySelector('.form-wrapper').classList.add('success');
+  }).catch(function(err) {
+    console.log('error!', err)
+  });
 });
